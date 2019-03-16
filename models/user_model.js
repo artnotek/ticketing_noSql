9dc39a7253045de0fda281fe7dbe15035ec7ccb6
dbebli <beblidaniel@gmail.com>
2019-03-16 21:52:30 +0100

    First commit

--- models/user_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = mongoose.Schema({
-   name: !String,
-   lastName: !String,
-   job: !String
-});
-
-module.exports = mongoose.model('User',UserSchema);
